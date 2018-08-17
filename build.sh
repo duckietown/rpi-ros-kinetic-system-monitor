c619c0d6c9afb0b7d67f34cb557569fadd0a1ad3
Andrea F. Daniele <afdaniele@ttic.edu>
2018-08-17 14:35:32 -0500

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t afdaniele/rpi-ros-kinetic-system-monitor:latest ./
-docker build -t afdaniele/rpi-ros-kinetic-system-monitor:standalone ./standalone/
+docker build -t duckietown/rpi-ros-kinetic-system-monitor:latest ./
+docker build -t duckietown/rpi-ros-kinetic-system-monitor:standalone ./standalone/
